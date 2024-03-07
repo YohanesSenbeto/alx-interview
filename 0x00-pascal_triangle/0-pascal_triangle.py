51d7f27fe081a982768086f4bad7367e0c6dc904
YohanesSenbeto <jonicasenbeto@gmail.com>
2024-03-07 21:01:33 +0000

    Pascal Triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,22 +1,20 @@
 def pascal_triangle(n):
     """
-        Generate Pascal's Triangle up to the nth row
-            """
+    Generate Pascal's Triangle up to the nth row
+    """
+    if n <= 0:
+        return []
 
+    triangle = [[1]]
 
-if n <= 0:
+    for i in range(1, n):
+        prev_row = triangle[-1]
+        new_row = [1]
 
-    return []
-triangle = [[1]]
+        for j in range(1, i):
+            new_row.append(prev_row[j - 1] + prev_row[j])
 
-for i in range(1, n):
-    prev_row = triangle[-1]
-new_row = [1]
+        new_row.append(1)
+        triangle.append(new_row)
 
-for j in range(1, i):
-    new_row.append(prev_row[j - 1] + prev_row[j])
-
-new_row.append(1)
-triangle.append(new_row)
-
-return triangle
+    return triangle
