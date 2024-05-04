b510d597c628326e3f59d5d007f0f86bacb73c56
YohanesSenbeto <jonicasenbeto@gmail.com>
2024-05-04 23:36:38 +0300

    UPdated make change pro

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -7,25 +7,21 @@ Making Change Problem
 def makeChange(coins, total):
     """
     Determines the fewest number of coins needed to meet a given amount total.
-
-    Args:
-        coins (list): A list of available coin denominations.
-        total (int): The total amount to make change for.
-
-    Returns:
-        int: The fewest number of coins needed to meet total, or -1 if no
-             combination of coins can sum up to total.
     """
     if total <= 0:
         return 0
 
-    coins = sorted(set(coins), reverse=True)
-    min_coins = [float("inf")] * (total + 1)
-    min_coins[0] = 0
+    c = sorted(set(coins), reverse=True)
+    dp = [float("inf")] * (total + 1)
+    dp[0] = 0
 
-    for coin in coins:
-        for amount in range(coin, total + 1):
-            a = min_coins[amount - coin] + 1
-            min_coins[amount] = min(min_coins[amount], a)
+    """
+    Iterate over each amount from 1 to total
+    and each coin to find the minimum number of coins
+    """
+    for a in range(1, total + 1):
+        for coin in c:
+            if a >= coin:
+                dp[a] = min(dp[a], dp[a - coin] + 1)
 
-    return min_coins[total] if min_coins[total] != float("inf") else -1
+    return dp[total] if dp[total] != float("inf") else -1
