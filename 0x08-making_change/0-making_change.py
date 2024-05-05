77de611b64e202b4bcd5b72be15ecf8a4a122d5e
YohanesSenbeto <jonicasenbeto@gmail.com>
2024-05-05 23:41:13 +0300

    Making change problem

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,21 +1,24 @@
 #!/usr/bin/python3
 
+"""Making Change Problem"""
+
 
 def makeChange(coins, total):
     """
     Determines the fewest number of coins needed to meet a given amount total.
     If total is 0 or less, return 0
-    If total cannot be met by any num of coins, return -1
+    If total cannot be met by any num of coins
     """
     if not coins or coins is None:
         return -1
     if total <= 0:
         return 0
-
-    memo = {0: 0}
-    for amount in range(1, total + 1):
-        memo[amount] = float("inf")
-        for coin in coins:
-            if amount - coin >= 0 and memo[amount - coin] != float("inf"):
-                memo[amount] = min(memo[amount], memo[amount - coin] + 1)
-    return memo[total] if memo[total] != float("inf") else -1
+    change = 0
+    coins = sorted(coins, reverse=True)
+    for coin in coins:
+        while coin <= total:
+            total -= coin
+            change += 1
+        if total == 0:
+            return change
+    return -1
